c859a3a0c4f3b8533ddf8a25d81bd828dc6cb9b9
Johan Infante <johaninft21@gmail.com>
2025-02-07 17:57:16 -0500

    ver 1.0.3 | Modificando Next.config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,35 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
-    domains: ["avatars.githubusercontent.com", "images.unsplash.com", "udemy-certificate.s3.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/57459541',
+        search: '?v=4',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/photo-1542831371-29b0f74f9713',
+        search: '?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbWFjaW9ufGVufDB8fDB8fHww',
+      },
+      {
+        protocol: 'https',
+        hostname: 'udemy-certificate.s3.amazonaws.com',
+        port: '',
+        pathname: '/account123/**',
+        search: '',
+      },
+    ],
   },
   i18n: {
     locales: ["en", "es"],
